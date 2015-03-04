481d2d069552c8be903200cabbe77899d6f6f4ba
LiaCubay <cubay82@hotmail.it>
2015-03-04 12:00:30 +0100

    Inserimento di una immagine nella GUI_Esempio e creato una nuova GUI

--- EsempioGWT/src/com/EsempioGWT/client/Gui/GUI_Esempio.java
+++ EsempioGWT/src/com/EsempioGWT/client/Gui/GUI_Esempio.java
@@ -4,20 +4,41 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
+import com.google.gwt.user.client.ui.HasHorizontalAlignment;
+import com.google.gwt.user.client.ui.HorizontalPanel;
+import com.google.gwt.user.client.ui.HasVerticalAlignment;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Image;
 
 public class GUI_Esempio extends Composite {
 
 	public GUI_Esempio(){
 		VerticalPanel vP = new VerticalPanel();
-		Button newB = new Button("Button");
-		Label lab = new Label("Label1");
-		Label lab1 = new Label("Label2");
+		vP.setSpacing(10);
+		Label lab = new Label("Esempio Pagina X");
 		
 		vP.add(lab);
-		vP.add(newB);
-		vP.add(lab1);
+		vP.setCellHorizontalAlignment(lab, HasHorizontalAlignment.ALIGN_CENTER);
+		
+		HorizontalPanel horizontalPanel = new HorizontalPanel();
+		horizontalPanel.setSpacing(5);
+		vP.add(horizontalPanel);
+		horizontalPanel.setWidth("450px");
+		Label lab1 = new Label("Hello utente");
+		horizontalPanel.add(lab1);
+		horizontalPanel.setCellVerticalAlignment(lab1, HasVerticalAlignment.ALIGN_MIDDLE);
+		horizontalPanel.setCellHorizontalAlignment(lab1, HasHorizontalAlignment.ALIGN_CENTER);
+		lab1.setHeight("28px");
+		Button newB = new Button("Button");
+		horizontalPanel.add(newB);
+		horizontalPanel.setCellHorizontalAlignment(newB, HasHorizontalAlignment.ALIGN_CENTER);
 		
 		initWidget(vP);
+		
+		Image image = new Image("gwt/clean/images/circles.png");
+		vP.add(image);
+		image.setSize("440px", "756px");
 		
 	}
 }
